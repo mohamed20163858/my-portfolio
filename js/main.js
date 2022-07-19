9d393909c25716346845b2488c517f657087c5f1
Mohamed Saleh <mohamed20163858@gmail.com>
2022-07-19 19:47:04 +0200

    fix ESLint errors part 1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-let menu = document.querySelector('header nav img');
-let main = document.querySelector('main');
-let header=document.querySelector('header');
-let ul=document.createElement('ul');
-ul.classList.add('menu-decoration','hidden');
-ul.innerHTML=`
+const menu = document.querySelector('header nav img');
+const main = document.querySelector('main');
+const header = document.querySelector('header');
+const ul = document.createElement('ul');
+ul.classList.add('menu-decoration', 'hidden');
+ul.innerHTML = `
 <img src="images/Close.png" alt="close-icon">
 <li><a href="#portoflio">Portoflio</a></li>
 <li><a href="#about">About</a></li>
 <li><a href="#contact">Contact</a></li>`;
 document.body.appendChild(ul);
-menu.addEventListener('click', function() {
-    ul.classList.remove('hidden');
-    main.classList.add('hidden');
-    header.classList.add('hidden');
-    
-    let closeTab=document.querySelector('.menu-decoration img');
-    let listItems=document.querySelectorAll('.menu-decoration li');
-    for(let i=0; i<listItems.length;++i)
+menu.addEventListener('click', function () {
+  ul.classList.remove('hidden');
+  main.classList.add('hidden');
+  header.classList.add('hidden');
+  let closeTab=document.querySelector('.menu-decoration img');
+  let listItems=document.querySelectorAll('.menu-decoration li');
+  for(let i=0; i<listItems.length;++i)
     {
         let li=listItems[i];
         li.addEventListener('click',function() {
@@ -25,10 +24,9 @@ menu.addEventListener('click', function() {
             header.classList.remove('hidden');
             });
     }
-    closeTab.addEventListener('click',function() {
-    ul.classList.add('hidden');
-    main.classList.remove('hidden');
-    header.classList.remove('hidden');
-    });
-    
+  closeTab.addEventListener('click',function() {
+  ul.classList.add('hidden');
+  main.classList.remove('hidden');
+  header.classList.remove('hidden');
+  }); 
 });
