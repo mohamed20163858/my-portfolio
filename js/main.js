846e5874091b0d9ef6a43071eaf4732f22d7f31d
Mohamed Saleh <mohamed20163858@gmail.com>
2022-07-19 20:24:18 +0200

    fix ESLint errors part 2

--- js/main.js
+++ js/main.js
@@ -9,24 +9,25 @@ ul.innerHTML = `
 <li><a href="#about">About</a></li>
 <li><a href="#contact">Contact</a></li>`;
 document.body.appendChild(ul);
-menu.addEventListener('click', function () {
+function addMenu() {
   ul.classList.remove('hidden');
   main.classList.add('hidden');
   header.classList.add('hidden');
-  let closeTab=document.querySelector('.menu-decoration img');
-  let listItems=document.querySelectorAll('.menu-decoration li');
-  for(let i=0; i<listItems.length;++i)
-    {
-        let li=listItems[i];
-        li.addEventListener('click',function() {
-            ul.classList.add('hidden');
-            main.classList.remove('hidden');
-            header.classList.remove('hidden');
-            });
-    }
-  closeTab.addEventListener('click',function() {
+}
+function removeMenu() {
   ul.classList.add('hidden');
   main.classList.remove('hidden');
   header.classList.remove('hidden');
-  }); 
-});
+}
+function menuAction() {
+  addMenu();
+  const closeTab=document.querySelector('.menu-decoration img');
+  const listItems=document.querySelectorAll('.menu-decoration li');
+  for(let i=0; i<listItems.length;++i)
+    {
+        let li=listItems[i];
+        li.addEventListener('click',removeMenu);
+    }
+  closeTab.addEventListener('click',removeMenu); 
+}
+menu.addEventListener('click',menuAction);
